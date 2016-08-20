b89673ba780675c12a32797f2a7949a3dc4fcca3
Stephen James <sajames1958@gmail.com>
2016-08-20 22:06:53 +0000

    nav bar

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -17,7 +17,7 @@ var WeatherForm = React.createClass({
       <div>
         <form onSubmit={this.onFormSubmit}>
         <input type="text" ref="location" placeholder="Enter City Name"/>
-        <button>Get Weather</button>
+        <button className="button" >Get Weather</button>
         </form>
       </div>      
     )
