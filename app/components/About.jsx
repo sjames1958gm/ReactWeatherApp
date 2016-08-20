5d7925e555df568bb4a88215c391e9917d07bf22
Stephen James <sajames1958@gmail.com>
2016-08-20 12:53:01 +0000

    changes

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,11 @@
 var React = require("react");
 
-var About = (props) => <h3>About Component</h3>
+var About = (props) => {
+  return (
+    <div>
+    <h3>About Weather</h3>
+    <p>This application returns the temperature for a given city</p>
+    </div>);
+}
 
 module.exports = About;
