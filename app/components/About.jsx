5b74113ca44b277ef23fe6b7318066f4779faa00
Stephen James <sajames1958@gmail.com>
2016-08-21 00:18:23 +0000

    more styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,10 @@ var React = require("react");
 var About = (props) => {
   return (
     <div>
-    <h3>About Weather</h3>
-    <p>This application returns the temperature for a given city</p>
+      <h1 className="text-center">About</h1>
+      <h4 className="text-center">A <a href="https://facebook.github.io/react" target="_blank">React</a> based weather application.</h4>
+      <p className="text-center">Using <a href="http://openweathermap.org" target="_blank">openweathermap</a> api.</p>
+      <p className="text-center">Clone the source at <a href="https://github.com/sjames1958gm/ReactWeatherApp" target="_blank">ReactWeatherApp</a></p>
     </div>);
 }
 
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 var Examples = (props) => {
   return (
     <div>
-    <h3>Weather Examples</h3>
-    <p>Examples are found here</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out.</p>
+      <ol>
+        <li>
+          <Link to="/?location=McKinney">McKinney, TX</Link>
+        </li>
+        <li>
+          <Link to="/?location=London">London, England</Link>
+        </li>
+      </ol>
     </div>
   )
 }
